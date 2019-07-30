94713548f18ee6dacb99f2b03110c947214a40ab
Timothy Emmanuel Kasasa <timothyk@laboremus.no>
2019-07-30 15:32:28 +0300

    Added vutify 2.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default Vue.extend({
-  name: 'home',
   components: {
     HelloWorld,
   },
